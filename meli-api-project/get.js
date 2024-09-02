10db177969259d7dad922fe86eea5d72b65360a2
MarcoATL <marko274.matl@gmail.com>
2024-09-02 00:35:31 -0600

    move_folder

--- meli-api-project/get.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('http://localhost:3000/top-1000-lowest-price')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
